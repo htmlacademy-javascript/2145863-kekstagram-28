d6e7cef6413bd9ead648ce00cee883d3b9d5dd2e
Alexander Fedorov <ru9.alex@gmail.com>
2023-02-17 17:27:11 +0300

    Удаляет консольный вывод

--- js/functions.js
+++ js/functions.js
@@ -41,29 +41,36 @@ const makePadStart = (str, minLength, pad) => {
   return str;
 };
 
-console.log('* isValidLenght *');
-console.log(isValidLenght('проверяемая строка', 20));
-console.log(isValidLenght('проверяемая строка', 18));
-console.log(isValidLenght('проверяемая строка', 10));
 
-console.log('* isPalindrome *');
-console.log(isPalindrome('топот'));
-console.log(isPalindrome('ДовОд'));
-console.log(isPalindrome('Кекс'));
-console.log(isPalindrome('Лёша на полке клопа нашёл '));
+isValidLenght('проверяемая строка', 20);
+isPalindrome('топот');
+extractNumbers('2023 год');
+makePadStart('1', 2, '0');
 
-console.log('* extractNumbers *');
-console.log(extractNumbers('2023 год'));
-console.log(extractNumbers('ECMAScript 2022'));
-console.log(extractNumbers('1 кефир, 0.5 батона'));
-console.log(extractNumbers('а я томат'));
-console.log(extractNumbers(2023));
-console.log(extractNumbers(-1));
-console.log(extractNumbers(1.5));
 
-console.log('* makePadStart *');
-console.log(makePadStart('1', 2, '0'));
-console.log(makePadStart('1', 4, '0'));
-console.log(makePadStart('q', 4, 'werty'));
-console.log(makePadStart('q', 4, 'we'));
-console.log(makePadStart('qwerty', 4, '0'));
+// console.log('* isValidLenght *');
+// console.log(isValidLenght('проверяемая строка', 20));
+// console.log(isValidLenght('проверяемая строка', 18));
+// console.log(isValidLenght('проверяемая строка', 10));
+
+// console.log('* isPalindrome *');
+// console.log(isPalindrome('топот'));
+// console.log(isPalindrome('ДовОд'));
+// console.log(isPalindrome('Кекс'));
+// console.log(isPalindrome('Лёша на полке клопа нашёл '));
+
+// console.log('* extractNumbers *');
+// console.log(extractNumbers('2023 год'));
+// console.log(extractNumbers('ECMAScript 2022'));
+// console.log(extractNumbers('1 кефир, 0.5 батона'));
+// console.log(extractNumbers('а я томат'));
+// console.log(extractNumbers(2023));
+// console.log(extractNumbers(-1));
+// console.log(extractNumbers(1.5));
+
+// console.log('* makePadStart *');
+// console.log(makePadStart('1', 2, '0'));
+// console.log(makePadStart('1', 4, '0'));
+// console.log(makePadStart('q', 4, 'werty'));
+// console.log(makePadStart('q', 4, 'we'));
+// console.log(makePadStart('qwerty', 4, '0'));
