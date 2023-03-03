6d86dca2c7bfa23b2c80805ec0a7700d2f66f93b
Keks <keks@htmlacademy.ru>
2023-03-03 13:06:31 +0300

    :heavy_check_mark: Сборка #6

---
+++ 6/js/render-thumbnails.js
@@ -0,0 +1,26 @@
+const pictures = document.querySelector('.pictures');
+const fragment = document.createDocumentFragment();
+
+const createThumbnail = ({id, url: src, comments, likes, description}) => {
+  const pictureNode = document.createElement('a');
+  pictureNode.href = '#';
+  pictureNode.classList.add('picture');
+  pictureNode.innerHTML = `
+    <img class="picture__img" src="${src}" width="182" height="182" data-id="${id}" alt="${description}">
+    <p class="picture__info">
+      <span class="picture__comments">${comments.length}</span>
+      <span class="picture__likes">${likes}</span>
+    </p>
+  `;
+
+  return pictureNode;
+};
+
+const renderThumbnails = (thumbnails) => {
+  thumbnails.forEach((element) => {
+    fragment.append(createThumbnail(element));
+  });
+  pictures.append(fragment);
+};
+
+export { renderThumbnails };
---
+++ 6/js/setup.js
@@ -0,0 +1,18 @@
+const PHOTOS_AMOUNT = 25; // Количество моковых объектов фотографий
+
+const Likes = {
+  MIN: 15,
+  MAX: 200,
+};
+
+const Avatar = {
+  MIN: 1,
+  MAX: 6,
+};
+
+const Message = {
+  MIN: 1,
+  MAX: 2,
+};
+
+export { PHOTOS_AMOUNT, Likes, Avatar, Message };
